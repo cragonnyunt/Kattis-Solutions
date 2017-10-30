172150194dbe3d9b0a3581f5ad343c5ff248c38c
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-10-30 13:38:31 +0630

    Update estate.java

--- java/estate.java
+++ java/estate.java
@@ -2,21 +2,20 @@ import java.util.Scanner;
 
 public class estate {
 	static int[] options = new int[1000000];
-	
+
 	static {
 		int count = 1;
-		for(int i = 2; i < options.length; i += count + 1)
-		{
-		    for (int j = i; j < options.length; j += count)
-			options[j - 1]++;
-		    count++;
+		for (int i = 2; i < options.length; i += count + 1) {
+			for (int j = i; j < options.length; j += count)
+				options[j - 1]++;
+			count++;
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int input;
-		while((input = scan.nextInt()) != 0) {
+		while ((input = scan.nextInt()) != 0) {
 			System.out.println(options[input - 1]);
 		}
 		scan.close();
