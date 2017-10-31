ea2fa2ed24c0cfe08ac6d3157031246fd7bec8bf
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-10-31 13:20:13 +0630

    Update arithmetic.java

--- java/arithmetic.java
+++ java/arithmetic.java
@@ -1,16 +1,24 @@
 import java.util.Scanner;
 
 public class arithmetic {
+	static String padLeft(String str, int length) {
+		return (new String(new char[length]).replace("\0", "0") + str).substring(str.length());
+	}
+
 	static String toBinary(String octal) {
 		if (octal.equals("0"))
 			return "0";
 		StringBuilder sb = new StringBuilder(3 * octal.length());
 		for (char ch : octal.toCharArray()) {
-			String binary_str = Integer.toBinaryString(Character.getNumericValue(ch));
-			sb.append(("000" + binary_str).substring(binary_str.length()));
+			int octal_data = Character.getNumericValue(ch);
+			String binary_string = Integer.toBinaryString(octal_data);
+			sb.append(padLeft(binary_string, 3));
 		}
-		int length = sb.length() + 4 - sb.length() % 4;
-		return (new String(new char[length]).replace("\0", "0") + sb).substring(sb.length());
+		int length = sb.length();
+		int extra_space;
+		if ((extra_space = sb.length() % 4) != 0)
+			length += 4 - extra_space;
+		return padLeft(sb.toString(), length);
 	}
 
 	static String toHex(String binary) {
@@ -18,8 +26,10 @@ public class arithmetic {
 			return "0";
 		StringBuilder sb = new StringBuilder(binary.length() / 4);
 		for (int i = 0; i < binary.length(); i += 4) {
-			String binary_str = binary.substring(i, i + 4);
-			sb.append(Integer.toHexString(Integer.parseInt(binary_str, 2)).toUpperCase());
+			String binary_string = binary.substring(i, i + 4);
+			int decimal_data = Integer.parseInt(binary_string, 2);
+			String hex_string = Integer.toHexString(decimal_data).toUpperCase();
+			sb.append(hex_string);
 		}
 		return sb.toString().replaceFirst("^0+(?!$)", "");
 	}
