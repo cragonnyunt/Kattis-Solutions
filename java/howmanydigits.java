9427649054d1f21e606152fcfd457f8568024564
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-10-31 14:44:16 +0630

    Create howmanydigits.java

---
+++ java/howmanydigits.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;;
+
+public class howmanydigits {
+	public static double[] bases = new double[1000001];
+
+	static {
+		bases[0] = bases[1] = 1;
+		for (int i = 2; i < bases.length; i++)
+			bases[i] = Math.log10(i) + bases[i - 1];
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		while (scan.hasNextInt()) {
+			int num = scan.nextInt();
+			System.out.println((int) Math.floor(bases[num]));
+		}
+		scan.close();
+	}
+}
