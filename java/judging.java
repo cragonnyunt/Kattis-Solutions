6c0aeaec6e7e01eb116fcff633896efd3f9a9e83
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-01 11:14:18 +0630

    Create judging.java

---
+++ java/judging.java
@@ -0,0 +1,31 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class judging {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		HashMap<String, Integer> domJudge = new HashMap<>(n);
+		for (int i = 0; i < n; i++) {
+			String result = scan.next();
+			if (domJudge.containsKey(result))
+				domJudge.put(result, domJudge.get(result) + 1);
+			else
+				domJudge.put(result, 1);
+		}
+		HashMap<String, Integer> kattisJudge = new HashMap<>(n);
+		for (int i = 0; i < n; i++) {
+			String result = scan.next();
+			if (kattisJudge.containsKey(result))
+				kattisJudge.put(result, kattisJudge.get(result) + 1);
+			else
+				kattisJudge.put(result, 1);
+		}
+		int count = 0;
+		for (String s : domJudge.keySet()) {
+			if (kattisJudge.containsKey(s))
+				count += Math.min(domJudge.get(s), kattisJudge.get(s));
+		}
+		System.out.println(count);
+	}
+}
