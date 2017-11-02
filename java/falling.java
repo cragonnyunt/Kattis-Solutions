0537734633abe4e7f6a0b1f2e5769958c2f1e4ee
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-02 11:59:50 +0630

    Create falling.java

---
+++ java/falling.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class falling {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int input = scan.nextInt();
+		int X = -1;
+		int minXSum = Integer.MAX_VALUE;
+		for (int i = 1; i <= Math.ceil(Math.sqrt(input)); i++) {
+			if (input % i == 0) {
+				int factor = input / i;
+				if ((i + factor) % 2 == 0)
+					if (minXSum > i + factor) {
+						minXSum = i + factor;
+						X = i;
+					}
+			}
+		}
+		if (X == -1) {
+			System.out.println("impossible");
+		} else {
+			int Y = input / X;
+			int a = (Y + X) / 2;
+			int b = (Y - X) / 2;
+			System.out.println(b + " " + a);
+		}
+		scan.close();
+	}
+}
