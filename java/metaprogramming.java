24c1eb76538462f1b34d8b45f9296de120663491
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-03 21:54:07 +0630

    Updated via Git Desktop

--- java/metaprogramming.java
+++ java/metaprogramming.java
@@ -5,7 +5,7 @@ public class metaprogramming {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		HashMap<String, Integer> meta = new HashMap<>();
-		while (scan.hasNext()) {
+		while (scan.hasNextLine()) {
 			String command = scan.nextLine();
 			String[] command_split = command.split(" ");
 			String cmd_type = command_split[0];
