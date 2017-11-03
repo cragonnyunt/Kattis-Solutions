a73dadb4c7bc2dd12f82dbb5ac336e5637361829
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-03 21:57:27 +0630

    Update metaprogramming.java

--- java/metaprogramming.java
+++ java/metaprogramming.java
@@ -2,33 +2,32 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class metaprogramming {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		HashMap<String, Integer> meta = new HashMap<>();
-		while (scan.hasNextLine()) {
-			String command = scan.nextLine();
-			String[] command_split = command.split(" ");
-			String cmd_type = command_split[0];
-			if (cmd_type.equals("define")) {
-				int x = Integer.parseInt(command_split[1]);
-				String var = command_split[2];
-				meta.put(var, x);
-			} else {
-				if (!meta.containsKey(command_split[1]) || !meta.containsKey(command_split[3]))
-					System.out.println("undefined");
-				else {
-					int x = meta.get(command_split[1]);
-					String y = command_split[2];
-					int z = meta.get(command_split[3]);
-					if (y.equals("<"))
-						System.out.println(x < z);
-					else if (y.equals(">"))
-						System.out.println(x > z);
-					else
-						System.out.println(x == z);
-				}
-			}
-		}
-		scan.close();
-	}
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        HashMap<String, Integer> meta = new HashMap<>();
+        while (scan.hasNextLine()) {
+            String[] command_split = scan.nextLine().split(" ");
+            String cmd_type = command_split[0];
+            if (cmd_type.equals("define")) {
+                int x = Integer.parseInt(command_split[1]);
+                String var = command_split[2];
+                meta.put(var, x);
+            } else {
+                if (!meta.containsKey(command_split[1]) || !meta.containsKey(command_split[3]))
+                    System.out.println("undefined");
+                else {
+                    int x = meta.get(command_split[1]);
+                    String y = command_split[2];
+                    int z = meta.get(command_split[3]);
+                    if (y.equals("<"))
+                        System.out.println(x < z);
+                    else if (y.equals(">"))
+                        System.out.println(x > z);
+                    else
+                        System.out.println(x == z);
+                }
+            }
+        }
+        scan.close();
+    }
 }
