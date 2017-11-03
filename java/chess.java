c59a955f8ffc1eddde63d9e72a828fcbab734e74
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-03 11:39:19 +0630

    Update chess.java

--- java/chess.java
+++ java/chess.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class chess2 {
+public class chess {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int T = scan.nextInt();
