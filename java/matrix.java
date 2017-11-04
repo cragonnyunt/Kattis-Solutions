9d017c023ea472ec2f26a96d5e68012ec083b2f1
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-04 16:47:48 +0630

    Create matrix.java

---
+++ java/matrix.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class matrix {
+	static int getX(int a, int b, int c, int d) {
+		return b / (c * b - a * d);
+	}
+
+	static int getY(int a, int b, int c, int d) {
+		return a / (a * d - b * c);
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int count = 1;
+		while (scan.hasNextInt()) {
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			int c = scan.nextInt();
+			int d = scan.nextInt();
+			int x = getX(c, d, a, b);
+			int y = getX(a, b, c, d);
+			int z = getY(c, d, a, b);
+			int w = getY(a, b, c, d);
+			System.out.println("Case " + count++ + ":");
+			System.out.println(x + " " + y);
+			System.out.println(z + " " + w);
+		}
+		scan.close();
+	}
+}
