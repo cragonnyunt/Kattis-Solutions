f7642bb53336802d42d943fb0c84635aad675466
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-05 22:04:53 +0630

    Create snapperhard.java

---
+++ java/snapperhard.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class snapperhard {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int T = scan.nextInt();
+		for (int i = 1; i <= T; i++) {
+			int N = scan.nextInt();
+			int K = scan.nextInt() + 1;
+			System.out.println("Case #" + i + ": " + (K % Math.pow(2, N) == 0 ? "ON" : "OFF"));
+		}
+		scan.close();
+	}
+}
