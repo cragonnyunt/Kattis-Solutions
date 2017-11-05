dcdc46fa66fa717545c2a22a3b49544c31222815
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-05 15:17:48 +0630

    Create artichoke.java

---
+++ java/artichoke.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class artichoke {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int p = scan.nextInt();
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		int d = scan.nextInt();
+		int n = scan.nextInt();
+		double prevMax = Double.MIN_VALUE;
+		double maxDecline = 0;
+		for (int i = 1; i <= n; i++) {
+			double price = p * (Math.sin(a * i + b) + Math.cos(c * i + d) + 2);
+			if (prevMax > price) {
+				maxDecline = Math.max(maxDecline, prevMax - price);
+			} else if (prevMax < price) {
+				prevMax = price;
+			}
+		}
+		System.out.println(maxDecline);
+		scan.close();
+	}
+}
