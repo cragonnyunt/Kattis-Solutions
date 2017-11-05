7d546aa4cdbf2ce7223670eb6c6821ccdc53427e
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-05 10:01:29 +0630

    Create natrij.java

---
+++ java/natrij.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class natrij {
+	static int getSec(String time) {
+		String[] t = time.split(":");
+		int time_sec = 0;
+		time_sec += 3600 * Integer.parseInt(t[0]);
+		time_sec += 60 * Integer.parseInt(t[1]);
+		time_sec += Integer.parseInt(t[2]);
+		return time_sec;
+	}
+
+	static String padLeft(int str) {
+		return ("00" + str).substring(String.valueOf(str).length());
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int time1 = getSec(scan.next());
+		int time2 = getSec(scan.next());
+		int time_difference = time2 - time1;
+		if (time_difference <= 0)
+			time_difference += 86400;
+		System.out.print(padLeft(time_difference / 3600) + ":");
+		System.out.print(padLeft(time_difference % 3600 / 60) + ":");
+		System.out.print(padLeft(time_difference % 60));
+		scan.close();
+	}
+}
