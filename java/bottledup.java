e544233497b345b7ba1ee85788696a7e62c06e70
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-06 13:52:35 +0630

    Create bottledup.java

---
+++ java/bottledup.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class bottledup {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int s = scan.nextInt();
+		int v1 = scan.nextInt();
+		int v2 = scan.nextInt();
+		int a = 0, b = 0;
+		for (int i = 0; i <= s / v1; i++) {
+			int j;
+			if ((s - v1 * i) % v2 == 0 && (i + (j = (s - v1 * i) / v2) < a + b || a + b == 0)) {
+				a = i;
+				b = j;
+			}
+		}
+		if (a + b == 0)
+			System.out.println("Impossible");
+		else
+			System.out.println(a + " " + b);
+		scan.close();
+	}
+}
