5ceea1285af92ed26de930452edf133afd0c4797
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-06 16:30:55 +0630

    Update shopaholic.java

--- java/shopaholic.java
+++ java/shopaholic.java
@@ -6,8 +6,8 @@ public class shopaholic {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		Long[] price= new Long[n];
-		while(n --> 0)
+		Long[] price = new Long[n];
+		while (n-- > 0)
 			price[price.length - n - 1] = scan.nextLong();
 		Arrays.sort(price, new Comparator<Long>() {
 			public int compare(Long o1, Long o2) {
@@ -15,9 +15,9 @@ public class shopaholic {
 			}
 		});
 		long discount = 0L;
-        for (int i = 2; i < price.length; i += 3)
-            discount += price[i];
-        System.out.println(discount);
+		for (int i = 2; i < price.length; i += 3)
+			discount += price[i];
+		System.out.println(discount);
 		scan.close();
 	}
 }
