ccba23e0076441ba0cafe1cd9bbe89bfc8d55267
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-06 21:23:09 +0630

    Update calculatingdartscores.java

--- java/calculatingdartscores.java
+++ java/calculatingdartscores.java
@@ -2,10 +2,11 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class calculatingdartscores {
-	static String[] dart = new String[181];
-	static int[][] dartArea = new int[27][3];
-	static final String[] area = { "single", "double", "triple" };
+	static final String[] dart = new String[181];
 	static {
+		final int[][] dartArea = new int[27][3];
+		final int[][] score = new int[9261][3];
+		final String[] area = { "single", "double", "triple" };
 		Arrays.fill(dart, "impossible");
 		int count = 0;
 		for (int a = 1; a <= 3; a++)
@@ -15,26 +16,35 @@ public class calculatingdartscores {
 					dartArea[count][1] = b;
 					dartArea[count++][2] = c;
 				}
+		count = 0;
 		for (int x3 = 0; x3 <= 20; x3++)
 			for (int x2 = 0; x2 <= 20; x2++)
 				for (int x1 = 0; x1 <= 20; x1++) {
-					for (int[] i : dartArea) {
-						int a = i[0];
-						int b = i[1];
-						int c = i[2];
-						int dartScore = x3 * a + x2 * b + x1 * c;
-						if (dart[dartScore] == "impossible") {
-							StringBuilder sb = new StringBuilder();
-							if (x3 != 0)
-								sb.append(String.format("%s %d\n", area[a - 1], x3));
-							if (x2 != 0)
-								sb.append(String.format("%s %d\n", area[b - 1], x2));
-							if (x1 != 0)
-								sb.append(String.format("%s %d", area[c - 1], x1));
-							dart[dartScore] = sb.toString();
-						}
-					}
+					score[count][0] = x3;
+					score[count][1] = x2;
+					score[count++][2] = x1;
 				}
+		for (int[] i : score) {
+			for (int[] j : dartArea) {
+				int a = j[0];
+				int b = j[1];
+				int c = j[2];
+				int x3 = i[0];
+				int x2 = i[1];
+				int x1 = i[2];
+				int dartScore = x3 * a + x2 * b + x1 * c;
+				if (dart[dartScore] == "impossible") {
+					StringBuilder sb = new StringBuilder();
+					if (x3 != 0)
+						sb.append(String.format("%s %d\n", area[a - 1], x3));
+					if (x2 != 0)
+						sb.append(String.format("%s %d\n", area[b - 1], x2));
+					if (x1 != 0)
+						sb.append(String.format("%s %d", area[c - 1], x1));
+					dart[dartScore] = sb.toString();
+				}
+			}
+		}
 	}
 
 	public static void main(String[] args) {
