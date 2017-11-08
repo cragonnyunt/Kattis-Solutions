3fec9e40bb9e7bfd0df6bdb16db823aceb5c4a1f
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-08 21:51:00 +0630

    Update doorman.java

--- java/doorman.java
+++ java/doorman.java
@@ -1,11 +1,21 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class doorman {
-	static club c = new club();
-	static Gender toGender(char c) {
-		return c == 'M' ? Gender.M : Gender.W;
+	static int Men_Count, Women_Count;
+	static char moreGender;
+	static ArrayList<Integer> trackedIndex = new ArrayList<>();
+	static int maxIndex = -1;
+
+	static void letinPeople(char g, int index) {
+		if (g == 'M')
+			Men_Count++;
+		else
+			Women_Count++;
+		trackedIndex.add(index);
+		if(maxIndex < index)
+			maxIndex = index;
+		moreGender = (Men_Count > Women_Count) ? 'M' : 'W';
 	}
 
 	public static void main(String[] args) {
@@ -14,50 +24,23 @@ public class doorman {
 		String queue = scan.next();
 		int index = 0;
 		while (index < queue.length()) {
-			if (c.getDifference() < X) {
-				if (!c.trackedIndex.contains(index))
-					c.letinPeople(toGender(queue.charAt(index)), index);
+			int difference = Math.abs(Men_Count - Women_Count);
+			if (difference < X) {
+				if (!trackedIndex.contains(index))
+					letinPeople(queue.charAt(index), index);
 				index++;
-			} else if (c.getDifference() == X) {
-				if (Math.abs(index - Collections.max(c.trackedIndex)) > 2)
+			} else if (difference == X) {
+				if (Math.abs(index - maxIndex) > 2)
 					break;
-				if (c.moreGender != toGender(queue.charAt(index)) && !c.trackedIndex.contains(index)) {
-					c.letinPeople(toGender(queue.charAt(index)), index);
+				if (moreGender != queue.charAt(index) && !trackedIndex.contains(index)) {
+					letinPeople(queue.charAt(index), index);
 					index = 0;
 				} else
 					index++;
 			} else
 				break;
 		}
-		System.out.println(c.getPeople());
+		System.out.println(Men_Count + Women_Count);
 		scan.close();
-	}
-}
-
-enum Gender {
-	M, W
-}
-
-class club {
-
-	int Men_Count, Women_Count;
-	public Gender moreGender;
-	public ArrayList<Integer> trackedIndex = new ArrayList<>();
-
-	public int getDifference() {
-		return Math.abs(Men_Count - Women_Count);
-	}
-
-	public int getPeople() {
-		return Men_Count + Women_Count;
-	}
-
-	public void letinPeople(Gender g, int index) {
-		if (g == Gender.M)
-			Men_Count++;
-		else
-			Women_Count++;
-		trackedIndex.add(index);
-		moreGender = (Men_Count > Women_Count) ? Gender.M : Gender.W;
 	}
 }
