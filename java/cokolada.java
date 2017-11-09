4b0bff995fb13ca8b90e3465455746640b25abb2
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-09 21:39:25 +0630

    Create cokolada.java

---
+++ java/cokolada.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class cokolada {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int K = scan.nextInt();
+		int breaks = 0;
+		int pow = 0;
+		boolean toCount = false;
+		while (K > 0) {
+			if (K % 2 == 1)
+				toCount = true;
+			if (toCount)
+				breaks++;
+			pow++;
+			K /= 2;
+		}
+		if (breaks == 1) {
+			pow--;
+			breaks = 0;
+		}
+		System.out.println((int)Math.pow(2, pow) + " " + breaks);
+		scan.close();
+	}
+}
