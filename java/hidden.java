c395ce518a5ebdca750c3e8febe78818cf4c3be7
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-10 13:35:14 +0630

    Create hidden.java

---
+++ java/hidden.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class hidden {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String password = scan.next();
+		String data = scan.next();
+		for (char c : data.toCharArray())
+			if (password.contains(String.valueOf(c)))
+				if (password.charAt(0) == c)
+					password = password.length() > 1 ? password.substring(1) : "";
+				else
+					break;
+		if (password.isEmpty())
+			System.out.println("PASS");
+		else
+			System.out.println("FAIL");
+		scan.close();
+	}
+}
