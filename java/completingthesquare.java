c8bcaa480e98b1f777f4ad7cc05c47613c65cdf8
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-12 15:20:41 +0630

    Create completingthesquare.java

---
+++ java/completingthesquare.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class completingthesquare {
+	static int getDistanceSqrt(int x1, int y1, int x2, int y2) {
+		int x = x1 - x2;
+		int y = y1 - y2;
+		return x * x + y * y;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int x1 = scan.nextInt();
+		int y1 = scan.nextInt();
+		int x2 = scan.nextInt();
+		int y2 = scan.nextInt();
+		int x3 = scan.nextInt();
+		int y3 = scan.nextInt();
+		if (getDistanceSqrt(x2, y2, x3, y3) == getDistanceSqrt(x1, y1, x2, y2) + getDistanceSqrt(x1, y1, x3, y3)) {
+			System.out.println((-x1 + x2 + x3) + " " + (-y1 + y2 + y3));
+		} else if (getDistanceSqrt(x1, y1, x3, y3) == getDistanceSqrt(x2, y2, x1, y1)
+				+ getDistanceSqrt(x2, y2, x3, y3)) {
+			System.out.println((x1 - x2 + x3) + " " + (y1 - y2 + y3));
+		} else {
+			System.out.println((x1 + x2 - x3) + " " + (y1 + y2 - y3));
+		}
+		scan.close();
+	}
+}
