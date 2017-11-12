8c350a54a1a35c2157ec34064e279b071534cb27
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-12 15:05:44 +0630

    Create weakvertices.java

---
+++ java/weakvertices.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class weakvertices {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n;
+		while ((n = scan.nextInt()) > 0) {
+			int[][] matrix = new int[n][];
+			for (int i = 0; i < n; i++) {
+				ArrayList<Integer> v = new ArrayList<>(n);
+				for (int j = 0; j < n; j++)
+					if (scan.nextInt() == 1)
+						v.add(j);
+				matrix[i] = new int[v.size()];
+				for (int j = 0; j < v.size(); j++)
+					matrix[i][j] = v.get(j);
+			}
+			for (int i = 0; i < n; i++) {
+				boolean isWeak = true;
+				for (int v : matrix[i]) {
+					OUT: for (int _v : matrix[v])
+						for (int _i : matrix[i])
+							if (_v == _i) {
+								isWeak = false;
+								break OUT;
+							}
+				}
+				if (isWeak)
+					System.out.print(i + " ");
+			}
+			System.out.println();
+		}
+		scan.close();
+	}
+}
