ee3474a8e78f1abf32772573f00d633be2232d87
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-12 21:18:08 +0630

    Create chopin.java

---
+++ java/chopin.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class chopin {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int cases = 1;
+		while (scan.hasNext()) {
+			String note = scan.next();
+			String tonality = scan.next();
+			if (note.length() == 1)
+				System.out.println("Case " + cases++ + ": UNIQUE");
+			else {
+				int inc = note.charAt(1) == '#' ? 1 : -1;
+				char ch = (char) ((note.charAt(0) + inc) % ('G' + 1));
+				if (note.charAt(0) == 'G' && inc == 1)
+					ch = 'A';
+				else if (note.charAt(0) == 'A' && inc == -1)
+					ch = 'G';
+				System.out
+						.println("Case " + cases++ + ": " + ch + (note.charAt(1) == '#' ? 'b' : '#') + " " + tonality);
+			}
+		}
+		scan.close();
+	}
+}
