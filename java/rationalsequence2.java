1dbf23830844616da2c9312f8ea5c36c1c47c7ef
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-14 15:48:04 +0630

    Create rationalsequence2.java

---
+++ java/rationalsequence2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class rationalsequence2 {
+	static int getN(int p, int q) {
+		if (p < q)
+			if (p == 1)
+				return (int) Math.pow(2, q - 1);
+			else
+				return getN(p, q % p) * (int) Math.pow(2, q / p);
+		else if (p > q)
+			return getN(p - q, p) + 1;
+		else
+			return 1;
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int P = scan.nextInt();
+		while (P-- > 0) {
+			int K = scan.nextInt();
+			String n = scan.next();
+			int p = Integer.parseInt(n.split("/")[0]);
+			int q = Integer.parseInt(n.split("/")[1]);
+			System.out.println(K + " " + getN(p, q));
+		}
+		scan.close();
+	}
+}
