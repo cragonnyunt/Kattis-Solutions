b2d10c204affa310e7983e186014bc6deca4c4a5
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-14 15:15:54 +0630

    Create sumkindofproblem.java

---
+++ java/sumkindofproblem.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sumkindofproblem {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int P = scan.nextInt();
+		while (P-- > 0) {
+			int K = scan.nextInt();
+			int N = scan.nextInt();
+			System.out.println(K + " " + N * (N + 1) / 2 + " " + N * N + " " + N * (N + 1));
+		}
+		scan.close();
+	}
+}
