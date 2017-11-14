49d0abff1d9726f2ca14d5ed91fb620f6fc45413
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-14 15:40:25 +0630

    Create acm.java

---
+++ java/acm.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class acm {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n;
+		ArrayList<Integer> time = new ArrayList<>();
+		ArrayList<Character> problem = new ArrayList<>();
+		ArrayList<Boolean> isRight = new ArrayList<>();
+		while ((n = scan.nextInt()) != -1) {
+			time.add(n);
+			problem.add(scan.next().charAt(0));
+			isRight.add(scan.next().equals("right"));
+		}
+		int prob_coun = 0, total_time = 0;
+		ArrayList<Character> solved = new ArrayList<Character>();
+		for (int i = 0; i < time.size(); i++)
+			if (isRight.get(i)) {
+				solved.add(problem.get(i));
+				prob_coun++;
+				total_time += time.get(i);
+			}
+		for (int i = 0; i < time.size(); i++)
+			if (!isRight.get(i) && solved.contains(problem.get(i)))
+				total_time += 20;
+		System.out.println(prob_coun + " " + total_time);
+		scan.close();
+	}
+}
