ee5a4268d343f4e1697e29e866dbeedeb582cdad
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-15 21:32:20 +0630

    Create mirror.java

---
+++ java/mirror.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class mirror {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int T = scan.nextInt();
+		for (int i = 1; i <= T; i++) {
+			int R = scan.nextInt();
+			int C = scan.nextInt();
+			char[][] image = new char[R][C];
+			for (int j = 0; j < R; j++) {
+				String line = scan.next();
+				for (int k = 0; k < C; k++)
+					image[j][k] = line.charAt(k);
+			}
+			System.out.println("Test " + i);
+			for (int j = R - 1; j >= 0; j--) {
+				for (int k = C - 1; k >= 0; k--)
+					System.out.print(image[j][k]);
+				System.out.println();
+			}
+		}
+		scan.close();
+	}
+}
