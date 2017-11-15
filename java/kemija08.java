41f54cfaa86e7f3ebf8e6a7f66a0e223d9149a36
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-15 21:27:31 +0630

    Create kemija08.java

---
+++ java/kemija08.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class kemija08 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		while (scan.hasNext()) {
+			String s = scan.next();
+			for (char c : new char[] { 'a', 'e', 'i', 'o', 'u' })
+				s = s.replaceAll(String.format("%cp%c", c, c), String.valueOf(c));
+			System.out.print(s + " ");
+		}
+		scan.close();
+	}
+}
