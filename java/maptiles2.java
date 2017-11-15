bdadfb867e817d90d312c58c24577fbec8e8e8f9
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-15 13:50:45 +0630

    Create maptiles2.java

---
+++ java/maptiles2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class maptiles2 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = scan.next();
+		int zoom = s.length();
+		int limit_x_start = 0;
+		int limit_x_end = (int) Math.pow(2, zoom) - 1;
+		int limit_y_start = limit_x_start;
+		int limit_y_end = limit_x_end;
+		for (char i : s.toCharArray()) {
+			switch (i) {
+			case '0':
+				limit_x_end = (limit_x_end + limit_x_start - 1) / 2;
+				limit_y_end = (limit_y_end + limit_y_start - 1) / 2;
+				break;
+			case '1':
+				limit_x_start = (limit_x_start + limit_x_end + 1) / 2;
+				limit_y_end = (limit_y_end + limit_y_start - 1) / 2;
+				break;
+			case '2':
+				limit_x_end = (limit_x_end + limit_x_start - 1) / 2;
+				limit_y_start = (limit_y_start + limit_y_end + 1) / 2;
+				break;
+			case '3':
+				limit_x_start = (limit_x_start + limit_x_end + 1) / 2;
+				limit_y_start = (limit_y_start + limit_y_end + 1) / 2;
+				break;
+			}
+		}
+		System.out.println(String.format("%d %d %d", zoom, limit_x_start, limit_y_start));
+		scan.close();
+	}
+}
