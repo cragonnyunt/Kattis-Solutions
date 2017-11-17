04e4cad5aa2dc14db6f587a3c047bf8d375d246a
Khin Nyunt <33210614+CragonNkhyut@users.noreply.github.com>
2017-11-17 13:12:35 +0630

    Created via GitHub Desktop

---
+++ java/areal.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class areal {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		double a = scan.nextDouble();
+		System.out.println(4 * Math.sqrt(a));
+		scan.close();
+	}
+}
